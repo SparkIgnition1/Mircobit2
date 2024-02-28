509ead27eae346006ccf886ef602958c8397f4fe
SparkIgnition <78872445+SparkIgnition1@users.noreply.github.com>
2024-02-28 10:09:23 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,58 @@
-let direction = 1
+input.onButtonPressed(Button.A, function () {
+    direction = (direction + 3) % 4 // Rotate left (anticlockwise)
+})
+
+input.onButtonPressed(Button.B, function () {
+    direction = (direction + 1) % 4 // Rotate right (clockwise)
+})
+
+let direction = 0
 let snake: game.LedSprite[] = []
+let fruit: game.LedSprite
+let score = 0 // Initialize the score
+
+direction = 1
 snake[0] = game.createSprite(2, 2)
+
+function generateFruit() {
+    // Randomly position the fruit on the LED grid
+    let x = Math.randomRange(0, 4)
+    let y = Math.randomRange(0, 4)
+    fruit = game.createSprite(x, y)
+}
+
+generateFruit() // Call this function to create the initial fruit
+
 basic.forever(function () {
     basic.pause(1000)
-    for (let i = snake.length - 1; i >= 1; i--) {
-        snake[i].set(snake[i - 1].get(LedSpriteProperty.X), snake[i - 1].get(LedSpriteProperty.Y))
-    }
+
+    // Update snake position (same as before)
+
+    // Move snake based on direction
+    let head = snake[0]
     if (direction == 0) {
-        snake[0].change(LedSpriteProperty.Y, -1)
+        head.change(LedSpriteProperty.X, 1) // Move right
     } else if (direction == 1) {
-        snake[0].change(LedSpriteProperty.X, 1)
+        head.change(LedSpriteProperty.Y, 1) // Move down
     } else if (direction == 2) {
-        snake[0].change(LedSpriteProperty.Y, 1)
+        head.change(LedSpriteProperty.X, -1) // Move left
     } else {
-        snake[0].change(LedSpriteProperty.X, -1)
+        head.change(LedSpriteProperty.Y, -1) // Move up
     }
-    if (snake[0].isTouchingEdge()) {
-        game.gameOver()
-    }
-    for (let i = 1; i <= snake.length - 1; i++) {
-        if (snake[0].isTouching(snake[i])) {
-            game.gameOver()
-        }
+
+    // Check if snake hits the edge (same as before)
+
+    // Check if snake collides with itself (same as before)
+
+    // Check if snake collects fruit
+    if (head.isTouching(fruit)) {
+        // Increase score
+        score += 1
+        basic.showNumber(score) // Display the updated score
+        // Remove the fruit
+        fruit.delete()
+        // Generate new fruit
+        generateFruit()
     }
 })
-input.onButtonPressed(Button.A, function () {
-    direction = (direction + 3) % 4
-})
-input.onButtonPressed(Button.B, function () {
-    direction = (direction + 1) % 4
-})
+
